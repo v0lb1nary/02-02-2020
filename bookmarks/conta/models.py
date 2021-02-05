ce976889a96f4f35de481234b22e03a6b47a8c5f
V0l_B1nary <volpatocursin@outlook.com>
2021-02-05 07:42:59 -0300

    sucss_test

--- bookmarks/conta/models.py
+++ bookmarks/conta/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Mixin(User):
-    doce_preferido = models.CharField(max_length=50)
+
+class Familia(models.Model):
+
+    mae = models.CharField(max_length = 50, blank = True)
+    pae = models.CharField(max_length = 50, blank = True)
+
+    class Meta:
+        abstract = True
+
+
+class Mixin(User, Familia):
+    doce_preferido = models.CharField(max_length = 50)
+
+    class Meta:
+        """Meta definition for MODELNAME."""
+        abstract = False
+        verbose_name = 'Mixin'
+        verbose_name_plural = 'Mixins'
+
+    def __str__(self):
+        """Unicode representation of MODELNAME."""
+        return self.get_full_name()
